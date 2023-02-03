3e44d8bfac12ca945fda00bbedb721ae82aead99
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-03 11:01:59 +0530

    added home page with HOC Component

--- client/src/components/PageHOC.jsx
+++ client/src/components/PageHOC.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { logo, heroImg } from "../assets";
+import styles from "../styles";
 
-const PageHOC = () => {
-  return <div>PageHOC</div>;
+const PageHOC = (Component, title, description) => () => {
+  return (
+    <div className={styles.hocContainer}>
+      <div class={styles.hocContentBox}>
+        <img
+          src={logo}
+          alt="logo"
+          className={styles.hocLogo}
+          onClick={() => navigate("/")}
+        />
+        <div class={styles.hocBodyWrapper}>
+          <div class="flex flex-row w-full">
+            <h1 className={`flex ${styles.headText} head-text`}>{title}</h1>
+            <p className={`${styles.normalText} my-10`}>{description}</p>
+
+            <Component />
+          </div>
+          <p className={styles.footerText}> Made by Shisuke Urahara</p>
+        </div>
+        <div class="flex flex-1">
+          <img
+            src={heroImg}
+            alt="hero-img"
+            className="w-full xl:h-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PageHOC;
